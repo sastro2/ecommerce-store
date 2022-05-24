c1f8e3ce150d2ec5876dce4070838057af550599
Sascha Stropek <sascha.stropek@gmail.com>
2022-05-24 20:06:50 +0200

    Created next app, Added products database, homepage and individual product pages

---
+++ pages/products/[productSlug].js
@@ -0,0 +1,24 @@
+import { productsDatabase } from '../../util/Database';
+
+export default function Product(props) {
+  return (
+    <main>
+      <h1>name: {props.product.name}</h1>
+      <p>IMAGE</p>
+      <p>price: {props.product.price}</p>
+      <p>description: {props.product.description}</p>
+    </main>
+  );
+}
+
+export function getServerSideProps(context) {
+  const foundProduct = productsDatabase.find((product) => {
+    return product.slug === context.query.productSlug;
+  });
+
+  return {
+    props: {
+      product: foundProduct,
+    },
+  };
+}
---
+++ util/Database.js
@@ -0,0 +1,78 @@
+import fs from 'node:fs';
+
+console.log(fs);
+
+export const productsDatabase = [
+  {
+    id: '1',
+    slug: '1',
+    name: '1',
+    imgPath: '',
+    price: '1',
+    description: '1',
+  },
+  {
+    id: '2',
+    slug: '2',
+    name: '2',
+    imgPath: '',
+    price: '2',
+    description: '2',
+  },
+  {
+    id: '3',
+    slug: '3',
+    name: '3',
+    imgPath: '',
+    price: '3',
+    description: '3',
+  },
+  {
+    id: '4',
+    slug: '4',
+    name: '4',
+    imgPath: '',
+    price: '4',
+    description: '4',
+  },
+  {
+    id: '5',
+    slug: '5',
+    name: '5',
+    imgPath: '',
+    price: '5',
+    description: '5',
+  },
+  {
+    id: '6',
+    slug: '6',
+    name: '6',
+    imgPath: '',
+    price: '6',
+    description: '6',
+  },
+  {
+    id: '7',
+    slug: '7',
+    name: '7',
+    imgPath: '',
+    price: '7',
+    description: '7',
+  },
+  {
+    id: '8',
+    slug: '8',
+    name: '8',
+    imgPath: '',
+    price: '8',
+    description: '8',
+  },
+  {
+    id: '9',
+    slug: '9',
+    name: '9',
+    imgPath: '',
+    price: '9',
+    description: '9',
+  },
+];
