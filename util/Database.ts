cb9e3468797907bad52adfa2677ae84c5f58d606
Sascha Stropek <sascha.stropek@gmail.com>
2022-06-04 19:11:43 +0200

    heroku

--- util/Database.ts
+++ util/Database.ts
@@ -2,21 +2,6 @@ import camelcaseKeys from 'camelcase-keys';
 import { config } from 'dotenv-safe';
 import postgres from 'postgres';
 
-module.exports = function setPostgresDefaultsOnHeroku() {
-  if (process.env.DATABASE_URL) {
-    const { parse } = require('pg-connection-string');
-
-    // Extract the connection information from the Heroku environment variable
-    const { host, database, user, password } = parse(process.env.DATABASE_URL);
-
-    // Set standard environment variables
-    process.env.PGHOST = host;
-    process.env.PGDATABASE = database;
-    process.env.PGUSERNAME = user;
-    process.env.PGPASSWORD = password;
-  }
-};
-
 config();
 
 const sql = postgres({ ssl: { rejectUnauthorized: false } });
